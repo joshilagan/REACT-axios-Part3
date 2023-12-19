3323e54f546bd1edd51f12f7c5207730693feaf1
joshi <joseph.ilagan.99@gmail.com>
2023-12-19 23:59:32 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// Import React, useState, useEffect, axios and Parser
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Parser } from "html-to-react";
 
-function App() {
+// Define the API endpoint
+const API_URL = 'https://api.tvmaze.com/schedule';
+
+// Define the component
+const App = () => {
+  // Use useState to store the posts data
+  const [posts, setPosts] = useState([]);
+
+  // Use useEffect to fetch the data when the component mounts
+  useEffect(() => {
+    // Define the async function that makes the request
+    const fetchData = async () => {
+      // Use axios to get the data from the API
+      const { data } = await axios.get(API_URL);
+      // Update the state with the data
+      setPosts(data);
+    };
+    // Invoke the function
+    fetchData();
+  }, []); // Pass an empty array as the second argument to run only once
+    // Return the JSX to render
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+    
+      {posts.length > 0 ? (
+        <div className="content">
+          {posts.map((post) => (
+            <div className="post" key={post.id}>
+              <h2>{post.show.name}</h2>
+              {/* convert html strings to react elements */}
+              <p>{Parser().parse(post.show.summary)}</p>
+              <img src={post.show.image.medium} alt="" />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p className="loading">Loading...</p>
+      )}
     </div>
   );
-}
+};
 
+// Export the component
 export default App;
